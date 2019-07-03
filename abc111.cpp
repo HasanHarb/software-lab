e1bc6b5667f8c41693b63970036289d17780e819
Hasan Harb <harb.hasn98@gmail.com>
2019-07-03 13:06:51 +0300

    create c++ file

---
+++ abc111.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main(int argc, char const *argv[])
+{
+	
+	string n; 
+	cin >> n ; 
+	cout << 10 - (n[0]-'0') << 10 - (n[1]-'0') << 10 - (n[2]-'0') << endl;
+
+	return 0;
+}
